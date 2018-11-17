9883d71260b68a0beff8c055583f253d4f7f3e54
Michael Gamatero <mgamatero@gmail.com>
2018-11-17 09:11:22 -0800

    update modal shows song info.  gotta fix actual saving though

--- controller/routes.js
+++ controller/routes.js
@@ -1,113 +1,171 @@
 var Music = require('../model/music');
-var _ = require ('lodash')
-var mongoose = require('mongoose') 
+var _ = require('lodash')
+var mongoose = require('mongoose')
 
 
 module.exports = function (app) {
+
+
+    app.post('/music', (req, res) => {
+        var body = _.pick(req.body, ['text', 'artist', 'notes', 'link']); //only takes these properties, not completedAt
+        var music = new Music({
+            text: req.body.text,
+            username: req.body.username,
+            artist: req.body.artist,
+            link: req.body.link,
+            notes: req.body.notes,
+            media: req.body.media
+        })
+        music.save().then((doc) =>
+            res.send(doc))
+    }, (e) => {
+        res.status(400).send()
+    })
+
+
+    app.get('/music', (req, res) => {
+        Music.find({ completed: "false" }).then((music) => {
+            res.json(music)
+        }, (e) => {
+            res.status(400).send()
+        })
+    })
+
+    app.get('/music/archived', (req, res) => {
+        Music.find({ completed: "true" }).then((music) => {
+            res.json(music)
+        }, (e) => {
+            res.status(400).send()
+        })
+    })
+
+
+    app.get('/music/:id', (req, res) => {
+        Music.findOne({ _id: req.params.id }).then((music) => {
+            res.json(music)
+        }, (e) => {
+            res.status(400).send()
+        })
+    })
+
+    // app.get('/music/?id', (req, res) => {
+    //     var convertedID = mongoose.Types.ObjectId(req.params.id)
+    //     // var convertedID = mongoose.Types.ObjectId(req.params.id)
+
+    //     if (!ObjectID.isValid(req.params.id)) {
+    //         return res.status(404).send()
+    //     }
+
+    //     Music.find({}).then((music) => {
+    //         if (!music) {
+    //             console.log('Die here')
+    //             return res.status(404).send()
+    //         }
+    //         res.json(music)
+    //     }, (e) => {
+    //         res.status(400).send()
+    //     })
+    // })
+
+
+    app.post('/music/delete/:id', (req, res) => {
+
+        Music.findOneAndRemove({ _id: req.params.id }).then((result) => {
+            if (!result) {
+                return res.status(404).send()
+            }
+            res.send({ result })
+        })
+    })
+
+    //  //--------------------------------------------------------------------
+    //     //THIS IS MEANT TO BE A PUT!!!!!!!!!!!!!!!!!!!!!!!!
+    //     //--------------------------------------------------------------------
+    //     app.post('/music/update/:id', function (req, res) {
+    //         db.scrape.update({ _id: req.params.id },
+    //             { $set: { notes: req.params.note } }).then(function (r) {
+    //                 res.send(r)
+    //             }).catch(function (e) {
+    //                 res.send(e)
+    //             })
+    //     })
+
+
+    //--------------------------------------------------------------------
+    //THIS IS MEANT TO BE A PUT!!!!!!!!!!!!!!!!!!!!!!!!
+    //--------------------------------------------------------------------
+    app.post('/music/update/:id', (req, res) => {
+
+
+        var music = new Music({
+            text: req.body.text,
+            username: req.body.username,
+            artist: req.body.artist,
+            link: req.body.link,
+            notes: req.body.notes,
+            media: req.body.media
+        })
+
+        Music.updateOne({ _id: req.params.id },
+            {
+                $set: {
+                    text: music.text,
+                    username: music.username,
+                    artist: music.artist,
+                    link: music.link,
+                    notes: music.notes,
+                    media: music.media 
+                    }
+            }, {new:true}
+        ).then((doc) => {
+            res.send(doc)
+        }, (e) => {
+            res.status(400).send()
+
+
+        })
+    })
+
+
+
+
+
+
+
     
 
-app.post('/music', (req, res) => {
-    var body = _.pick(req.body, ['text', 'artist','notes', 'link']); //only takes these properties, not completedAt
-    var music = new Music({
-        text: req.body.text,
-        username: req.body.username,
-        artist: req.body.artist,
-        link: req.body.link,
-        notes: req.body.notes,
-        media: req.body.media
-    })
-    music.save().then((doc) =>
-        res.send(doc))
-}, (e) => {
-    res.status(400).send()
-})
 
 
-app.get('/music', (req, res) => {
-    Music.find({completed:"false"}).then((music) => {
-        res.json(music)
-    }, (e) => {
-        res.status(400).send()
-    })
-})
 
-app.get('/music/archived', (req, res) => {
-    Music.find({completed:"true"}).then((music) => {
-        res.json(music)
-    }, (e) => {
-        res.status(400).send()
-    })
-})
+    app.patch('/music/:id', (req, res) => {
+        var id = req.params.id;
+        var body = _.pick(req.body, ['text', 'completed']); //only takes these properties, not completedAt
 
-
-app.get('/music/:id', (req, res) => {
-    Music.findOne({ _id:req.params.id}).then((music) => {
-        res.json(music)
-    }, (e) => {
-        res.status(400).send()
-    })
-})
-
-// app.get('/music/?id', (req, res) => {
-//     var convertedID = mongoose.Types.ObjectId(req.params.id)
-//     // var convertedID = mongoose.Types.ObjectId(req.params.id)
-
-//     if (!ObjectID.isValid(req.params.id)) {
-//         return res.status(404).send()
-//     }
-    
-//     Music.find({}).then((music) => {
-//         if (!music) {
-//             console.log('Die here')
-//             return res.status(404).send()
-//         }
-//         res.json(music)
-//     }, (e) => {
-//         res.status(400).send()
-//     })
-// })
-
-
-app.post('/music/delete/:id', (req, res) => {
-
-    Music.findOneAndRemove({ _id:req.params.id}).then((result) => {
-        if (!result) {
+        //valid id?
+        if (!ObjectID.isValid(req.params.id)) {
             return res.status(404).send()
         }
-        res.send({ result })
-   })
-})
 
-app.patch('/music/:id', (req, res) => {
-    var id = req.params.id;
-    var body = _.pick(req.body, ['text', 'completed']); //only takes these properties, not completedAt
-
-    //valid id?
-    if (!ObjectID.isValid(req.params.id)) {
-        return res.status(404).send()
-    }
-
-    //if boolean and completed = true
-    if ((_.isBoolean) && body.completed) {
-        body.completedAt = new Date().getTime();
-    } 
-    else 
-    {
-        body.completed = false;
-        body.completedAt = null;
-    }
-
-    //actual update happens here
-    //1st arg - id, 2nd is body, 3rd is a parameter that says return new updated obj
-    Music.findByIdAndUpdate(id, { $set: body }, { new: true }).then((music) => {
-        if (!music) {
-            return res.status(404).send()
+        //if boolean and completed = true
+        if ((_.isBoolean) && body.completed) {
+            body.completedAt = new Date().getTime();
         }
-        res.send({ music })
-    }).catch((e) => {
-        rest.statsus(400).send()
-    })
+        else {
+            body.completed = false;
+            body.completedAt = null;
+        }
 
-});
+        //actual update happens here
+        //1st arg - id, 2nd is body, 3rd is a parameter that says return new updated obj
+        Music.findByIdAndUpdate(id, { $set: body }, { new: true }).then((music) => {
+            if (!music) {
+                return res.status(404).send()
+            }
+            res.send({ music })
+        }).catch((e) => {
+            rest.statsus(400).send()
+        })
+
+    });
 
 }
