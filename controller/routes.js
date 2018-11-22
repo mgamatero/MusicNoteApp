5bbc47f2d62e46b99bab76156c29984311005111
Michael Gamatero <mgamatero@gmail.com>
2018-11-22 15:18:52 -0800

    details modal for archived.html ok now, shows lyrics

--- controller/routes.js
+++ controller/routes.js
@@ -10,23 +10,9 @@ var lyr = require('lyrics-fetcher')
 module.exports = function (app) {
 
 
-    app.post('/music', (req, res) => {
-        var body = _.pick(req.body, ['text', 'artist', 'notes', 'link']); //only takes these properties, not completedAt
-        var music = new Music({
-            text: req.body.text,
-            username: req.body.username,
-            artist: req.body.artist,
-            link: req.body.link,
-            notes: req.body.notes,
-            media: req.body.media
-        })
-        music.save().then((doc) =>
-            res.send(doc))
-    }, (e) => {
-        res.status(400).send()
-    })
-
-
+    //------------------------------------------------------------------------------------
+    // Route that gets all songs with completed:FALSE.  These in effect are Active songs
+    //------------------------------------------------------------------------------------
     app.get('/music', (req, res) => {
         Music.find({ completed: "false" }).then((music) => {
             res.json(music)
@@ -35,6 +21,10 @@ module.exports = function (app) {
         })
     })
 
+
+    //------------------------------------------------------------------------------------
+    // Route that gets all songs with completed:TRUE.  These in effect are Archived songs
+    //------------------------------------------------------------------------------------
     app.get('/music/archived', (req, res) => {
         Music.find({ completed: "true" }).then((music) => {
             res.json(music)
@@ -44,6 +34,9 @@ module.exports = function (app) {
     })
 
 
+    //-----------------------------------------------------
+    // Route that gets song based on ID
+    //-----------------------------------------------------
     app.get('/music/:id', (req, res) => {
         Music.findOne({ _id: req.params.id }).then((music) => {
             res.json(music)
@@ -54,8 +47,10 @@ module.exports = function (app) {
 
 
 
+    //-----------------------------------------------------
+    // Route that deletes the song.  
+    //-----------------------------------------------------
     app.post('/music/delete/:id', (req, res) => {
-
         Music.findOneAndRemove({ _id: req.params.id }).then((result) => {
             if (!result) {
                 return res.status(404).send()
@@ -64,33 +59,26 @@ module.exports = function (app) {
         })
     })
 
-
-    app.post('/music/complete/:id', (req, res) => {
-
-        Music.findOneAndUpdate({ _id: req.params.id },
-            { $set: { completed: true } }).then((result) => {
-                if (!result) {
-                    return res.status(404).send()
-                }
-                res.send({ result })
-            })
+    //-----------------------------------------------------
+    // Route to post new song
+    //-----------------------------------------------------
+    app.post('/music', (req, res) => {
+        var body = _.pick(req.body, ['text', 'artist', 'notes', 'link']); //only takes these properties, not completedAt
+        var music = new Music({
+            text: req.body.text,
+            username: req.body.username,
+            artist: req.body.artist,
+            link: req.body.link,
+            notes: req.body.notes
+        })
+        music.save().then((doc) =>
+            res.send(doc))
+    }, (e) => {
+        res.status(400).send()
     })
-
-
-    app.post('/music/activate/:id', (req, res) => {
-
-        Music.findOneAndUpdate({ _id: req.params.id },
-            { $set: { completed: false } }).then((result) => {
-                if (!result) {
-                    return res.status(404).send()
-                }
-                res.send({ result })
-            })
-    })
-
 
     //--------------------------------------------------------------------
-    //THIS IS MEANT TO BE A PUT!!!!!!!!!!!!!!!!!!!!!!!!
+    //Route that Updates song properties
     //--------------------------------------------------------------------
     app.post('/music/update/:id', (req, res) => {
         console.log(req)
@@ -102,7 +90,6 @@ module.exports = function (app) {
                     artist: req.body.artist,
                     link: req.body.link,
                     notes: req.body.notes,
-                    media: req.body.media 
                 }
             }
         ).then((result) => {
@@ -113,32 +100,42 @@ module.exports = function (app) {
         })
     })
 
+    //-----------------------------------------------------
+    // Route that sets the song completed attribute to true.  
+    //-----------------------------------------------------
+    app.post('/music/complete/:id', (req, res) => {
+        Music.findOneAndUpdate({ _id: req.params.id },
+            { $set: { completed: true } }).then((result) => {
+                if (!result) {
+                    return res.status(404).send()
+                }
+                res.send({ result })
+            })
+    })
 
+    //-----------------------------------------------------
+    // Route that sets the song completed attribute to false.  
+    //-----------------------------------------------------
+    app.post('/music/activate/:id', (req, res) => {
+        Music.findOneAndUpdate({ _id: req.params.id },
+            { $set: { completed: false } }).then((result) => {
+                if (!result) {
+                    return res.status(404).send()
+                }
+                res.send({ result })
+            })
+    })
+
+
+
+    //-----------------------------------------------------------
+    // Route that gets song lyrics.  Lyrics-fetcher npm used here
+    //-----------------------------------------------------------
 
     app.get('/lyrics/:artist/:song', (req, res) => {
-        lyr.fetch(req.params.artist, req.params.song, function (err, lyrics) {
-            console.log(err || lyrics);
+            lyr.fetch(req.params.artist, req.params.song, function (err, lyrics) {
             res.send(err || lyrics)
         });
-        // mx.search(req.params.song, song =>  {
-        //     if(song.length === 0) {
-        //         res.json("Empty")
-        //         console.log(song.length + 'none')
-        //     }
-        //     else{
-        //         console.log(song.length  + 'some')
-                
-        //     mx.get(song[0].url,lyric=>{
-        //         console.log(lyric)
-        //         res.json(lyric)
-        //     })
-        // }
-            
-        // }
-        //         , (e) => {
-        //     res.status(400).send()
-        // })
-        
     })
 
 }
