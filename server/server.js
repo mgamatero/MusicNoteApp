66c8ef1d322079cae101e94e24d4bfd6bc199bc6
Michael Gamatero <mgamatero@gmail.com>
2019-01-03 12:55:06 -0800

    correction on server.js.  added hbs app.set('view engine','hbs').  Causing heroku fail

--- server/server.js
+++ server/server.js
@@ -21,11 +21,12 @@ var path = require('path')
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
-app.set('view engine', )  //for hbs
+app.set('view engine', 'hbs' )  //for hbs
 app.use(express.static(path.join(__dirname,'../public')))
 
 const hbs = require('hbs')  //for hbs
 hbs.registerPartials(path.join(__dirname,'../views/partials'))
+// hbs.registerPartials(express.static(__dirname + '../views/partials'))
 
 //Routes
 require('../controller/routes')(app)
